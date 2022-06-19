ab42572f8971a36356cefa7bfba613edc7fa5d89
bai21018 <bai21018@byui.edu>
2022-06-18 20:17:22 -0600

    barebones game

--- game.py
+++ game.py
@@ -1,1 +1,26 @@
 #this is the hilo game
+import random as r
+
+player_points = int(600)
+
+random_number = r.randint(1, 13)
+
+continue_playing = input("Would you like to play? Type 'y' for yes and 'n' for no: ")
+player_guess = input("Enter a number: ")
+
+
+
+while continue_playing != "n":
+    if int(player_guess) > random_number:
+        print(f"You guessed '{player_guess}' incorrectly and have lost 75 points")
+        player_points += int(-75)
+    else:
+        print(f"You guessed '{player_guess}' and have been awarded 100 points!")
+        player_points = player_points + int(100)
+
+        
+print(random_number)
+print(player_points)
+
+
+    
